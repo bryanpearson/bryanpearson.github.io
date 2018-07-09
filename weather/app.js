e43772be250439ef8f177c2eb79080a28f91a29a
Bryan Pearson <bryanmpearson@gmail.com>
2018-07-09 08:05:23 -0700

    weather page updates

--- weather/app.js
+++ weather/app.js
@@ -1,19 +1,63 @@
 var app = new Vue({
   el: '#app',
   data: {
-    station_list: ['KLGD', 'KORLAGRA7', 'KORLAGRA9', 'KORLAGRA11', 'KORUNION2', 'KORCOVE8', 'KORIMBLE2'],
-    stations: []
+    station_KLGD: null,
+    station_KORLAGRA7: null,
+    station_KORLAGRA9: null,
+    station_KORLAGRA11: null,
+    station_KORUNION2: null,
+    station_KORCOVE8: null,
+    station_KORIMBLE2: null
   },
 
   mounted() {
     this.$nextTick(function () {
       // Code that will run only after the
       // entire view has been rendered
-      for (i=0; i < this.station_list.length; i++) {
-        console.log(this.station_list[i])
-        axios.get("http://api.wunderground.com/api/6455e4cdcf958bbd/conditions/q/pws:" + this.station_list[i] +".json")
-          .then(response => { this.stations.push(response.data.current_observation)  })
-      }
+      // for (i=0; i < this.station_list.length; i++) {
+      //   console.log(this.station_list[i])
+      //   axios.get("http://api.wunderground.com/api/6455e4cdcf958bbd/conditions/q/pws:" + this.station_list[i] +".json")
+      //     .then(response => { this.stations.push(response.data.current_observation)  })
+      // }
+
+
+      axios.get("http://api.wunderground.com/api/6455e4cdcf958bbd/conditions/q/pws:KLGD.json")
+        .then(response => { this.station_KLGD = response.data.current_observation })
+      axios.get("http://api.wunderground.com/api/6455e4cdcf958bbd/conditions/q/pws:KORLAGRA7.json")
+        .then(response => { this.station_KORLAGRA7 = response.data.current_observation })
+      axios.get("http://api.wunderground.com/api/6455e4cdcf958bbd/conditions/q/pws:KORLAGRA9.json")
+        .then(response => { this.station_KORLAGRA9 = response.data.current_observation })
+      axios.get("http://api.wunderground.com/api/6455e4cdcf958bbd/conditions/q/pws:KORLAGRA11.json")
+        .then(response => { this.station_KORLAGRA11 = response.data.current_observation })
+      axios.get("http://api.wunderground.com/api/6455e4cdcf958bbd/conditions/q/pws:KORUNION2.json")
+        .then(response => { this.station_KORUNION2 = response.data.current_observation })
+      axios.get("http://api.wunderground.com/api/6455e4cdcf958bbd/conditions/q/pws:KORCOVE8.json")
+        .then(response => { this.station_KORCOVE8 = response.data.current_observation })
+      axios.get("http://api.wunderground.com/api/6455e4cdcf958bbd/conditions/q/pws:KORIMBLE2.json")
+        .then(response => { this.station_KORIMBLE2 = response.data.current_observation })
+
+      // axios.get("http://api.wunderground.com/api/6455e4cdcf958bbd/conditions/q/pws:KLGD.json")
+      //   .then(response => { this.stations['KLGD'] = response.data.current_observation })
+      // axios.get("http://api.wunderground.com/api/6455e4cdcf958bbd/conditions/q/pws:KORLAGRA7.json")
+      //   .then(response => { this.stations['KORLAGRA7'] = response.data.current_observation })
+      // axios.get("http://api.wunderground.com/api/6455e4cdcf958bbd/conditions/q/pws:KORLAGRA9.json")
+      //   .then(response => { this.stations['KORLAGRA9'] = response.data.current_observation })
+      // axios.get("http://api.wunderground.com/api/6455e4cdcf958bbd/conditions/q/pws:KORLAGRA7.json")
+      //   .then(response => { this.stations.push(response.data.current_observation)  })
+      // axios.get("http://api.wunderground.com/api/6455e4cdcf958bbd/conditions/q/pws:KORLAGRA9.json")
+      //   .then(response => { this.stations.push(response.data.current_observation)  })
+      // axios.get("http://api.wunderground.com/api/6455e4cdcf958bbd/conditions/q/pws:KORLAGRA11.json")
+      //   .then(response => { this.stations.push(response.data.current_observation)  })
+      // axios.get("http://api.wunderground.com/api/6455e4cdcf958bbd/conditions/q/pws:KORUNION2.json")
+      //   .then(response => { this.stations.push(response.data.current_observation)  })
+      // axios.get("http://api.wunderground.com/api/6455e4cdcf958bbd/conditions/q/pws:KORCOVE8.json")
+      //   .then(response => { this.stations.push(response.data.current_observation)  })
+      // axios.get("http://api.wunderground.com/api/6455e4cdcf958bbd/conditions/q/pws:KORIMBLE2.json")
+      //   .then(response => { this.stations.push(response.data.current_observation)  })
+
+
+
+
     })
   },
 
